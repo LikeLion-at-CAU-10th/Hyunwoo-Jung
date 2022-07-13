eef6168f6a7225dd1d75d616fa7f80bb785fae5f
supersett <nolang3748@naver.com>
2022-07-14 01:11:22 +0900

    add Todo models

--- BE_track/todomate/models.py
+++ BE_track/todomate/models.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.db import models
 from django.forms import CharField, DateField, NullBooleanField
 
@@ -7,3 +8,12 @@ class Category(models.Model):
     view_auth = models.IntegerField(default=0,null=True, blank=None)
     color = models.CharField(max_length=10, default='#000000',null=True, blank=None)
     pup_date = models.DateField(auto_now_add=True)
+    
+
+class Todo(models.Model):
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,null=False,blank=False)
+    content=models.CharField(max_length=50)
+    thumb_nail=models.ImageField(upload_to="todo/",null=True,blank=True)
+    is_completed=models.BooleanField(default=False)
+    pup_date=models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now=True)
